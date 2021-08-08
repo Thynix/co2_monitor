84df8bcccaadb275c1d3c668b3289b94891fd634
Steve Dougherty <steve@asksteved.com>
2021-08-08 19:50:08 -0400

    Purple during startup; add description line
    This also removes intermediate references to global values, like CO2
    measurement, having top level pass them in instead.

--- co2_to_color.cpp
+++ co2_to_color.cpp
@@ -39,13 +39,17 @@ const uint16_t leftmost_x = 2;
 
 void displayMessage(const char* message);
 void blink(uint32_t color);
-void set_color_from_co2();
-void print_data();
-uint32_t get_co2_color();
+void set_color_from_co2(float CO2);
+void print_data(float CO2, float temperature, float relative_humidity);
+uint32_t get_co2_color(float CO2);
+const char* get_co2_description(float CO2);
 
 void setup() {
   pixels.begin();
   pixels.setBrightness(DIM);
+  pixels.setPixelColor(0, PURPLE);
+
+  pinMode(ready_pin, INPUT);
 
   while (!scd30.begin()) {
     blink(PURPLE);
@@ -59,9 +63,6 @@ void setup() {
   pixels.clear();
   pixels.show();
 
-  pinMode(ready_pin, INPUT);
-  LowPower.attachInterruptWakeup(digitalPinToInterrupt(ready_pin), NULL, HIGH);
-
   display = Adafruit_SSD1306(SSD1306_LCDWIDTH, SSD1306_LCDHEIGHT, &Wire);
   while (!display.begin()) {
     blink(ORANGE);
@@ -72,6 +73,8 @@ void setup() {
   display.setTextColor(SSD1306_WHITE);
   display.clearDisplay();
   display.display();
+
+  LowPower.attachInterruptWakeup(digitalPinToInterrupt(ready_pin), NULL, RISING);
 }
 
 void loop() {
@@ -84,15 +87,15 @@ void loop() {
     return;
   }
 
-  print_data();
-  set_color_from_co2();
+  print_data(scd30.CO2, scd30.temperature, scd30.relative_humidity);
+  set_color_from_co2(scd30.CO2);
 
   LowPower.idle();
 }
 
-void set_color_from_co2() {
+void set_color_from_co2(float CO2) {
   static uint32_t previous_color = 0;
-  uint32_t current_color = get_co2_color();
+  uint32_t current_color = get_co2_color(CO2);
 
   // Brighten on change
   if (previous_color != current_color)
@@ -106,47 +109,66 @@ void set_color_from_co2() {
   pixels.show();
 }
 
-void print_data() {
+void print_data(float CO2, float temperature, float relative_humidity) {
   display.clearDisplay();
 
   // SCD-30
   // Measurement range: 400 ppm – 10.000 ppm
   // Accuracy: ±(30 ppm + 3%)
   display.setCursor(leftmost_x, 0);
-  display.print(scd30.CO2, 0);
+  display.print(CO2, 0);
   display.print(" ppm CO2");
 
   // SHT31
   // Accuracy 0.3 C
-  display.setCursor(leftmost_x, 10);
-  display.print(scd30.temperature, 1);
-  display.print(" C");
+  display.setCursor(leftmost_x, 8);
+  display.print(temperature, 1);
+  display.print(" C | ");
+  display.print(temperature * (9.0/5) + 32, 1);
+  display.print(" F");
 
   // SHT31
   // Accuracy ±2% - 4% at 90%+
-  display.setCursor(leftmost_x, 20);
-  display.print(scd30.relative_humidity, 0);
-  display.print("% humidity");
+  display.setCursor(leftmost_x, 16);
+  display.print(relative_humidity, 0);
+  display.print("% relative humidity");
+
+  display.setCursor(leftmost_x, 24);
+  display.print(get_co2_description(CO2));
 
   display.display();
 }
 
 /*
- * 250-400ppm      Normal background concentration in outdoor ambient air
- * 400-1,000ppm    Concentrations typical of occupied indoor spaces with good air exchange
- * 1,000-2,000ppm  Complaints of drowsiness and poor air.
+ * 350-400ppm      Normal background concentration in outdoor ambient air
+ * 400-900ppm      Well-ventilated occupied indoor space
+ * 900-2,000ppm    Complaints of drowsiness and poor air.
  * 2,000-5,000 ppm Headaches, sleepiness and stagnant, stale, stuffy air. Poor concentration, loss of attention, increased heart rate and slight nausea may also be present.
  * 5,000           Workplace exposure limit (as 8-hour TWA) in most jurisdictions.
- * >40,000 ppm     Exposure may lead to serious oxygen deprivation resulting in permanent brain damage, coma, even death.
+ * >40,000 ppm     Immediately harmful due to oxygen deprivation.
+ * 
+ * Working from
+ * https://www.dhs.wisconsin.gov/chemical/carbondioxide.htm
+ * https://www.epa.gov/sites/default/files/2014-08/documents/base_3c2o2.pdf
  */
-uint32_t get_co2_color() {
-  if (scd30.CO2 < 250) return WHITE;
-  if (scd30.CO2 < 400) return BLUE;
-  if (scd30.CO2 < 1000) return GREEN;
-  if (scd30.CO2 < 2000) return ORANGE;
-  if (scd30.CO2 < 5000) return RED;
+uint32_t get_co2_color(float CO2) {
+  if (CO2 < 350) return  WHITE;
+  if (CO2 < 400) return  BLUE;
+  if (CO2 < 900) return  GREEN;
+  if (CO2 < 2000) return ORANGE;
+  if (CO2 < 5000) return RED;
 
   return MAGENTA;
+}
+
+const char* get_co2_description(float CO2) {
+  if (CO2 < 350) return  "Very good outdoor";
+  if (CO2 < 400) return  "Typical outdoor";
+  if (CO2 < 900) return  "Well-ventilated indoor";
+  if (CO2 < 2000) return "Poor air";
+  if (CO2 < 5000) return "Stale, stuffy air";
+
+  return                 "> exposure limit";
 }
 
 void blink(uint32_t color) {
